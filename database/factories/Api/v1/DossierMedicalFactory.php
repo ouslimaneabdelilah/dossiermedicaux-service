37d8f041fc70f5a7a2f1cad7a70fbaa0f6e36a71
ouslimaneabdelilah <abdelilah.ouslimane@gmail.com>
2026-03-11 10:29:08 +0000

    faet: Add initial project with docker

---
+++ database/factories/Api/v1/DossierMedicalFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories\Api\v1;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Api\v1\DossierMedical>
+ */
+class DossierMedicalFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
